863a8b080b336bb4514d7d0e73422d116c21a57f
nikhil <nikhilgaikwad4691@gmail.com>
2026-09-03 18:08:15 +0530

    new change in color

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     });
 
     res.end(`
-        <body style="background-color: #6394e3;">
+        <body style="background-color: #d65de3;">
             <h1>Hello from Node.js team by Nikhil   !!!!</h1>
             <p>Application deployed using GitHub Actions → AWS EC2</p>
         </body>
